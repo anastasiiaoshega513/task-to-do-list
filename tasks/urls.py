6d94e2e6e61e878613b0f8dc727bdbda393a0b2f
Steicy <anastasiaosega@gmail.com>
2026-02-13 16:44:35 +0200

    updated index page

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import (
-    index,
+    TaskListView,
 )
 urlpatterns = [
-    path("", index, name="index"),
+    path("", TaskListView.as_view(), name="index"),
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from tasks.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "tasks/index.html"
+    context_object_name = "task_list"

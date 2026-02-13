c21bcf2b4d10031820aaa8e062ea22ac50537435
Steicy <anastasiaosega@gmail.com>
2026-02-13 17:35:07 +0200

    updated tag list page^ added create, update, delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from .views import (
     TaskListView,
     TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
 )
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "tasks"

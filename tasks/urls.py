236eb36ac733258dc1b492316b1f2b4c2f0d570a
Steicy <anastasiaosega@gmail.com>
2026-02-13 18:06:04 +0200

    updated index (task list) page

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,9 +5,16 @@ from .views import (
     TagCreateView,
     TagUpdateView,
     TagDeleteView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView, task_toggle,
 )
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("<int:pk>/toggle/", task_toggle, name="task-toggle"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 
 from tasks.models import Task, Tag
 
 
+def task_toggle(request, pk):
+    task = Task.objects.get(pk=pk)
+    if request.method == "POST":
+        task.is_done = not task.is_done
+        task.save(update_fields=["is_done"])
+    return redirect(reverse("tasks:index"))
+
+
 class TaskListView(generic.ListView):
     model = Task
     template_name = "tasks/index.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:index")
 
 
 class TagListView(generic.ListView):
